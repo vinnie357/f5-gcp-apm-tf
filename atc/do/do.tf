2d048dc39a93c86a8bdd8f7eb79534d9b5d842b2
vinnie <vinnie.mazza@gmail.com>
2020-04-17 15:38:14 -0400

    revert outputs

--- atc/do/do.tf
+++ atc/do/do.tf
@@ -1,7 +1,7 @@
 provider "bigip" {
-  address = "https://${var.mgmt_ip}"
-  username = "${var.adminUsername.value}"
-  password = "${var.password.value}"
+  address = var.bigip_mgmt_ips.value
+  username = var.adminUsername.value
+  password = var.password.value
 }
 
 #Declarative Onboarding template 01
@@ -9,15 +9,15 @@ data "template_file" "vm01_do_json" {
   template = "${file("${var.bigipLicense1 != "" ? "standalone_byol" : "standalone"}.json")}"
 
   vars = {
-    local_host      = "${var.host1_name}"
-    dns_server	    = "${var.dns_server}"
-    ntp_server	    = "${var.ntp_server}"
-    timezone	    = "${var.timezone}"
-    bigipLicense  = "${var.bigipLicense1}"
+    local_host      = var.host1_name
+    dns_server	    = var.dns_server
+    ntp_server	    = var.ntp_server
+    timezone	    = var.timezone
+    bigipLicense  = var.bigipLicense1
   }
 }
 
 resource "bigip_do"  "do-apm" {
      do_json = data.template_file.vm01_do_json.rendered
-     tenant_name = "apm"
+     tenant_name = "sample_test1"
  }
