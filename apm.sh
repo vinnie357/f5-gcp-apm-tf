7ce5700c59273b55cd0984c2220e2330039aee66
vinnie <vinnie.mazza@gmail.com>
2020-04-21 11:09:17 -0400

    add better wait for mgmt flip

--- apm.sh
+++ apm.sh
@@ -6,17 +6,30 @@ terraform init
 terraform plan
 #
 terraform apply --auto-approve
-echo "wait 6 minutes"
-sleep 360
 cd atc/do
 terraform output -state=../../terraform.tfstate --json > apm.auto.tfvars.json
+mgmtIp=$(cat apm.auto.tfvars.json | jq -r .instance01Info.value.network_interface[1].access_config[0].nat_ip)
+echo "wait max 10 minutes"
+checks=0
+while [[ "$checks" -lt 4 ]]; do
+    echo "waiting on: https://$mgmtIp" 
+    curl -sk --retry 15 --retry-connrefused --retry-delay 10 https://$mgmtIp
+if [ $? == 0 ]; then
+    echo "mgmt ready"
+    break
+fi
+echo "mgmt not ready yet"
+let checks=checks+1
+sleep 10
+done
 terraform init
 terraform plan
 terraform apply --auto-approve
 sleep 10
+# run again because of DO DHCLIENT bug
 terraform apply --auto-approve
-echo "wait 5 minutes"
-sleep 300
+echo "wait 2 minutes"
+sleep 120
 cp apm.auto.tfvars.json ../as3/apm.auto.tfvars.json
 cd ../as3
 terraform init
