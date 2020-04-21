4168db1c217859c20c1070a1f42c027fbfa8beef
vinnie <vinnie.mazza@gmail.com>
2020-04-21 09:44:41 -0400

    limit special characters

--- apm.tf
+++ apm.tf
@@ -2,7 +2,7 @@
 resource "random_password" "password" {
   length           = 16
   special          = true
-  override_special = " #%*+,-./:=?@[]^_~"
+  override_special = " %*+,-./:=?@[]_~"
 }
 
 module "bigip" {
