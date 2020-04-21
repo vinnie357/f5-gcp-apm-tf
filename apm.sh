9916fdb53e52277dfd6b746c61ab2f76571f4cad
vinnie <vinnie.mazza@gmail.com>
2020-04-21 11:24:18 -0400

    remove dos profile from afm

--- apm.sh
+++ apm.sh
@@ -11,9 +11,9 @@ terraform output -state=../../terraform.tfstate --json > apm.auto.tfvars.json
 mgmtIp=$(cat apm.auto.tfvars.json | jq -r .instance01Info.value.network_interface[1].access_config[0].nat_ip)
 echo "wait max 10 minutes"
 checks=0
-while [[ "$checks" -lt 4 ]]; do
+while [[ "$checks" -lt 2 ]]; do
     echo "waiting on: https://$mgmtIp" 
-    curl -sk --retry 15 --retry-connrefused --retry-delay 10 https://$mgmtIp
+    curl -sk --retry 10 --retry-connrefused --retry-delay 30 https://$mgmtIp
 if [ $? == 0 ]; then
     echo "mgmt ready"
     break
