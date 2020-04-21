08474d2c424c9f1f55a0244616b552b126588644
vinnie <vinnie.mazza@gmail.com>
2020-04-21 13:59:32 -0400

    wait longer for apm provisioning

--- apm.sh
+++ apm.sh
@@ -11,7 +11,7 @@ terraform output -state=../../terraform.tfstate --json > apm.auto.tfvars.json
 mgmtIp=$(cat apm.auto.tfvars.json | jq -r .instance01Info.value.network_interface[1].access_config[0].nat_ip)
 echo "wait max 10 minutes"
 checks=0
-while [[ "$checks" -lt 2 ]]; do
+while [[ "$checks" -lt 10 ]]; do
     echo "waiting on: https://$mgmtIp" 
     curl -sk --retry 10 --retry-connrefused --retry-delay 30 https://$mgmtIp
 if [ $? == 0 ]; then
@@ -20,7 +20,7 @@ if [ $? == 0 ]; then
 fi
 echo "mgmt not ready yet"
 let checks=checks+1
-sleep 10
+sleep 60
 done
 terraform init
 terraform plan
@@ -28,8 +28,8 @@ terraform apply --auto-approve
 sleep 10
 # run again because of DO DHCLIENT bug
 terraform apply --auto-approve
-echo "wait 2 minutes"
-sleep 120
+echo "wait 3 minutes"
+sleep 180
 cp apm.auto.tfvars.json ../as3/apm.auto.tfvars.json
 cd ../as3
 terraform init
